15a477b2b065675bb708f7077b01c6175403718d
Renato Reis <email@renatoamreis.com>
2021-01-14 20:27:20 +0100

    Adding project files

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+// Config variables that we will use in several places, and we only need to change here if necessary
+
+export const API_URL ='https://forkify-api.herokuapp.com/api/v2/recipes'
+export const TIMEOUT_SEC = 10
